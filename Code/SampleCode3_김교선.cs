46234479a1065d49f94ec62f299ca0fd0ccacae8
DESKTOP\kyosun <kyosun123@naver.com>
2020-05-13 13:03:08 +0900

    기존코드수정
    수정

---
+++ Code/SampleCode3_김교선.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 운영툴로 부터 이벤트 미션들을 한 그룹으로 만드는 클래스
+public class ContentsEventGroup
+{
+    public string Name { get; private set; }                    // 이벤트 이름
+    public uint EventKey { get; private set; }                  // 이벤트 고유키
+    public EContentsEvent ContentsType { get; private set; }    // 이벤트 타입
+    public DateTime StartDate { get; private set; }             // 이벤트 시작날짜
+    public DateTime EndDate { get; private set; }               // 이벤트 종료날짜
+    public DataDungeon OpenDungeon { get; private set; }        // 이벤트 던전체크
+
+    // 미션 정보 리스트
+    public List<ContentsEventInfo> EventInfoList { get; } = new List<ContentsEventInfo>();
+
+    private const string PARSE_STRING_DATE = "yyyy-MM-dd HH:mm:ss";
+
+    // 초기화
+    private void Initialize()
+    {
+        EventInfoList.Clear();
+        StartDate = DateTime.MinValue;
+        EndDate = DateTime.MinValue;
+    }
+
+    // 기본정보셋팅
+    public ContentsEventGroup(Hashtable data)
+    {
+        EventInfoList.Clear();
+
+        // 셋팅
+        Name = data["name"].ToString();
+        EventKey = uint.Parse(data["eventKey"].ToString());
+        uint contentsId = uint.Parse(data["eventType"].ToString());
+        ContentsType = (EContentsEvent)contentsId;
+        OpenDungeon = DataDungeon.GetByID(data["openDungeonID"].ToString());
+        StartDate = DateTime.ParseExact((string)data["startDateTime"], PARSE_STRING_DATE, null);
+        EndDate = DateTime.ParseExact((string)data["endDateTime"], PARSE_STRING_DATE, null);
+
+        // 기간별 셋팅
+        Hashtable dayTable = hash["dayTable"] as Hashtable;
+        for (int day = (int)EDayCountOfWeek.Day1; day <= (int)EDayCountOfWeek.Day7; day++)
+        {
+            // 해당 요일 이벤트 체크
+            if (dayTable.ContainsKey(day.ToString()) == false)
+            {
+                continue;
+            }
+
+            ContentsEventInfo eventInfo = new ContentsEventInfo(key, dayTable[day.ToString()]);
+            EventInfoList.Add(eventInfo);
+        }
+    }
+
+    // 이용가능 체크
+    public bool IsAvailiable()
+    {
+        if (DungeonHelper.IsCleared(OpenDungeon) || TimeHelper.IsInCurrent(StartDate, EndDate))
+        {
+            return true;
+        }
+        
+        return false;
+    }
+}
---
+++ Code/SampleCode4_김교선.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialHelper
+{
+    public enum EState
+    {
+        None, // 없음 혹은 종료
+        Wait, // 대기
+        Ready, // 준비
+        Play, // 플레이
+    }
+
+    static EState nowState = EState.None;
+    static EState beforeState = EState.None;
+
+    static Dictionary<ETutorialGroup, TutorialInfo> dicTutorialInfo = new Dictionary<ETutorialGroup, TutorialInfo>(); // 튜토리얼 그룹별 상태 저장
+
+    // 튜토리얼 데이터 Dic으로 셋팅(key:그룹)
+    static public void SetTutorialDatas()
+    {
+        dicTutorialInfo.Clear();
+        Dictionary<uint, DataTutorial> dicDatas = DataTutorial.GetDicDataTutorial();
+        foreach (DataTutorial data in dicDatas.Values)
+        {
+            ETutorialGroup groupType = (ETutorialGroup)data.GetGROUP_TYPE();
+            if (groupType != ETutorialGroup.None)
+            {
+                if (dicTutorialInfo.ContainsKey(groupType)) // 같은 그룹이라면 데이터만 추가
+                {
+                    dicTutorialInfo[groupType].AddDataList(data);
+                }
+                else // 없는 그룹이면 생성
+                {
+                    dicTutorialInfo.Add(groupType, new TutorialInfo(data, false));
+                }
+            }
+        }
+    }
+
+    // 클리어 그룹 체크
+    static public bool IsClearByGroup(ETutorialGroup group)
+    {
+        if (dicTutorialInfo.ContainsKey(group))
+        {
+            return dicTutorialInfo[group].IsComplete;
+        }
+        else
+        {
+            Debug.Log("Not Tutorial Info Group:" + group.ToString());
+        }
+        return false;
+    }
+
+    // 현재 해당 튜토리얼이 플레이 중인지
+    static public bool IsPlaying(ETutorialGroup group)
+    {
+        DataTutorial data = GetNowData();
+        if (data != null)
+        {
+            ETutorialGroup dataGroup = (ETutorialGroup)data.GetGROUP_TYPE();
+            return dataGroup == group;
+        }
+        return false;
+    }
+
+    // 리스트 정보 가져오기
+    static public List<TutorialInfo> GetInfoList(ETutorialGroup group)
+    {
+        List<TutorialInfo> list = new List<TutorialInfo>();
+        if (dicTutorialInfo.ContainsKey(group)) // 같은 그룹이라면 데이터만 추가
+        {
+            list = dicTutorialInfo[group];
+        }
+        return list;
+    }
+
+    // 상태 셋팅
+    static public void SetState(EState state)
+    {
+        if (nowState == state)
+        {
+            Debug.Log("Same State Group:" + group.ToString());
+            return;
+        }
+
+        beforeState = nowState;
+        nowState = state;
+    }
+
+    // 상태 가져오기
+    static public bool IsNowState(EState state)
+    {
+        return nowState == state;
+    }
+}
